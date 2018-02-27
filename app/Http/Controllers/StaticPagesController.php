f9400fd3ce8015d55735e7e5585a5796666ef523
vagrant <vagrant@homestead>
2018-02-27 15:28:15 +0000

    111

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    public function home(){
+    public function home()
+    {
         return view('static_pages/home');
     }
 
